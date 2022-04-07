2a77c5f8f1251bcd0dcd2e5aa0ec234b62993bd1
gavrinol <102408582+gavrinol@users.noreply.github.com>
2022-04-07 21:43:16 +0300

    Сделанные задачи

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,66 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Задание 1, 2
+
+        int[] apples = new int[3];
+        for (int i = 0; i < apples.length; i++) {
+            apples[i] = i + 1;
+            System.out.print(apples[i]);
+            if (i < apples.length - 1)
+                System.out.print(", ");
+        }
+        System.out.println();
+
+        double[] pineapples = {1.57, 7.654, 9.986};
+        for (int i = 0; i < pineapples.length; i++) {
+            System.out.print(pineapples[i]);
+            if (i < pineapples.length - 1)
+                System.out.print(", ");
+        }
+        System.out.println();
+
+        String[] alphabet = {"a", "b", "c", "d", "e", "f"};
+        System.out.print(String.join(", ", alphabet));
+        System.out.println();
+
+
+        //Задание 3
+
+        for (int i = apples.length - 1; i >= 0; i--) {
+            System.out.print(apples[i]);
+            if (i > 0)
+                System.out.print(", ");
+        }
+        System.out.println();
+
+        for (int i = pineapples.length - 1; i >= 0; i--) {
+            System.out.print(pineapples[i]);
+            if (i > 0)
+                System.out.print(", ");
+        }
+        System.out.println();
+
+        for (int i = alphabet.length - 1; i >= 0; i--) {
+            System.out.print(alphabet[i]);
+            if (i > 0)
+                System.out.print(", ");
+        }
+        System.out.println();
+
+        // Задание 4
+        for (int i = 0; i < apples.length; i++) {
+            if (apples[i] % 2 == 1) {
+                apples[i] = i + 2;
+            }
+            System.out.print(apples[i]);
+            if (i < pineapples.length - 1)
+                System.out.print(", ");
+        }
     }
 }
